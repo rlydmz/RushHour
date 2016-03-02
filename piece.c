6eb7eb09c3a6905a0c993689eb64ab5cbf79df7f
Raphael <reallydrumz@gmail.com>
2016-03-02 11:29:21 +0100

    Added files via upload

--- piece.c
+++ piece.c
@@ -4,9 +4,9 @@
 
 struct piece_s{
 
-    //x représente l'abscisse du coup supérieur gauche de la piece
+    //x représente l'abscisse du coté inférieur gauche de la piece
     int x;
-    //y représente lordonnee du coup superieur gauche de la piece
+    //y représente lordonnee du coté inférieur gauche de la piece
     int y;
     //si estPetite == true, la piece sera petite, sinon et sera grande
     bool estPetite;
@@ -65,15 +65,49 @@ void move_piece (piece p, dir d, int distance){
 }
 
 bool intersect(cpiece p1, cpiece p2){
+/*
+    if(p1!=NULL && p2!=NULL){
+
+        int nbCoteEnCommun = 0;
+        int hauteurp1 = get_height(p1);
+        int hauteurp2 = get_height(p2);
+        int largeurp1 = get_width(p1);
+        int largeurp2 = get_width(p2);
+
+        int cptMax1, cptMax2;
+
+        if(hauteurp1==2 || largeurp1==2)
+            cptMax1 = 6;
+        else if(hauteurp1==3 || largeurp1==3)
+            cptMax1 = 8;
+
+        if(hauteurp2==2 || largeurp2==2)
+            cptMax2 = 6;
+        else if(hauteurp2==3 || largeurp2==3)
+            cptMax2 = 8;
+
+        for(int i=0; i<cptMax1; i++){
+            for(int j=0; j<cptMax2; j++){
 
 
+               if(get_x(p1) == get_x(p2) && get_y(p1) == get_y(p2))
+                    nbCoteEnCommun++;
 
+            }
+        }
+
+
+    }
+*/
 }
+
 
 int get_x(cpiece p){
 
     if(p!=NULL)
         return (*p).x;
+
+    return 0;
 
 }
 
@@ -82,6 +116,7 @@ int get_y(cpiece p){
     if(p!=NULL)
         return (*p).y;
 
+    return 0;
 }
 
 int get_height(cpiece p){
@@ -97,6 +132,27 @@ int get_height(cpiece p){
         }
 
     }
+
+    return 0;
+}
+
+int get_width(cpiece p){
+
+    if(p!=NULL){
+        if((*p).bougeHorizontalement ==  true){
+            if((*p).estPetite ==  true)
+                return 2;
+            else
+                return 3;
+        }
+
+        else{
+                return 1;
+        }
+
+    }
+
+    return 0;
 }
 
 }
