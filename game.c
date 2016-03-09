eb3ed75871d93675391ce42ebe887e8ed41acf02
Eabril <emilio.abril@etu.u-bordeaux.fr>
2016-03-09 12:06:48 +0100

    Added files via upload

--- game.c
+++ game.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "game.h"
+
+#ifndef _GAME_H_
+#define _GAME_H_
+#include <stdbool.h>
 #include "piece.h"
 
-
-struct game_s{
+typedef struct game_s{
+    int width;
+    int height;
     int moves;
     int nb_pieces;
     piece p[];
 };
 
-
-game new_game_hr(int nb_pieces, piece *pieces){
-    game g = malloc(sizeof(struct game_s));
-    g->moves = 0;
-    g->nb_pieces = nb_pieces;
-    for (int i=0; i<nb_pieces ;i++){
-        g->p[i] = pieces[i];
-        copy_piece(g->p[i],pieces[i]);
-    }
-    return g;
-}
+//game new_game_hr(int nb_pieces, piece *pieces){
+//    game g = malloc(sizeof(struct game_s));
+//    g->moves = 0;
+//    g->nb_pieces = nb_pieces;
+//    for (int i=0; i<nb_pieces ;i++){
+//        g->p[i] = pieces[i];
+//        copy_piece(g->p[i],pieces[i]);
+//    }
+//    return g;
+//}
 
 void delete_game (game g){
-    free(g);
+    if(g!=NULL)
+        free(g);
 }
 
-/**
- * @brief Clone the game dst into the game dst.
- * @param src the game to copy
- * @param dst the copied game.
- */
 void copy_game (cgame src, game dst){
     dst->moves = src->moves;
     dst->nb_pieces = src->nb_pieces;
@@ -39,29 +36,16 @@ void copy_game (cgame src, game dst){
     }
 }
 
-/**
- * @brief Return the number of pieces on the game board g
-*/
 int game_nb_pieces(cgame g){
     return g->nb_pieces;
 }
 
-
-/**
- * @brief Return the piece of g with number bum
- * @param piece_num the num of the piece. This value must be between 0 and game_nb_pieces(g)-1.
-*/
 cpiece game_piece(cgame g, int piece_num){
     if(piece_num>game_nb_pieces(g))
         fprintf(stderr,"Error, place not in the game");
     return g->p[piece_num];
 }
 
-
-/**
- * @brief Test is the game is over (i.e. if the piece 0 has reached the exit).
- * @return true if the piece number 0 has coordinates (4,3)
-*/
 bool game_over_hr(cgame g){
     int x = get_x(g->p[0]);
     int y = get_y(g->p[0]);
@@ -72,20 +56,6 @@ bool game_over_hr(cgame g){
 
 }
 
-
-/**
- * @brief Tries to move the piece of number piece_num in direction d for distance cells.
- *
- * If the move is valid, then corresponding piece is moved, the number of moves of the game is incremented by distance and the returned value is true.
- * If the move is not valid, the game g remains unchanged and the function returns false.
- * A move is valid if:
- *   1) the piece stays in the game board,
- *   2) the direction is compatible is the type of the piece,
- *   3) the piece do not cross any other piece during its movement.
- * @param distance length of the move. This value must be strictly positive.
- * @param piece_num the number of the piece to move. This value must be between 0 and game_nb_pieces(g)-1.
- * @return true if the move is valid, false otherwise.
-*/
 //bool play_move(game g, int piece_num, dir d, int distance){
 //    piece p2 = new_piece_rh(10,10,true,true);
 //    copy_piece(g->p[piece_num],p2);
@@ -107,9 +77,6 @@ bool game_over_hr(cgame g){
 //        intersect(p2,g->p[i]);
 //        j++;
 //    }
-//
-//
-//
 //    }
 //    else if(!is_horizontal(g->p[piece_num])){
 //        if(d!=UP || d!=DOWN)
@@ -119,12 +86,47 @@ bool game_over_hr(cgame g){
 //
 //}
 
-
-/**
- * @brief Return the number of moves this the beginning of the game g.
- * Remind that move of a single piece of k cells in a single direction counts for k.
- */
 int game_nb_moves(cgame g){
     return g->moves;
 }
+
+
+
+///////////// version 2 /////////////////
+
+game new_game (int width, int height, int nb_pieces, piece *pieces){
+    game g = malloc(sizeof(struct game_s));
+    g->width = width;
+    g->height = height;
+    g->moves = 0;
+    g->nb_pieces = nb_pieces;
+    for (int i=0; i<nb_pieces ;i++){
+        g->p[i] = pieces[i];
+        copy_piece(g->p[i],pieces[i]);
+    }
+    return g;
+}
+
+int game_width(cgame g){
+    return g->width;
+}
+
+int game_height(cgame g){
+    return g->height;
+}
+
+int game_square_piece (game g, int x, int y){
+    if(x<0 || x>(g->witdth)-1 || y<0 || y>(g->height)-1) return -1; // carre hors de la grille
+
+    int num_piece = -1; // variable de retour
+    for(int i=0 ; i<(g->nb_pieces);i++){ // on teste toutes les pieces du jeu
+        if(get_x(game_piece(g,i))<x || get_y(game_piece(g,i))<y) continue; // carre a gauche ou en dessous de la piece i
+        // carre au dessus de la piece ou a droite de la piece i sans intersect
+        else if(x > get_x(game_piece(g,i))+get_width(game_piece(g,i)) || y > get_y(game_piece(g,i))+get_height(game_piece(g,i))) continue;
+        else num_piece = i; // seul cas restant, le carre est sur la piece i (et donc sur aucune autre piece)
+    }
+
+    return num_piece; // -1 si
+}
+
 
